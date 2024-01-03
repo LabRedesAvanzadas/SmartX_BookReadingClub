7082f38d71b019d7824b8c04bf259b8452c9d422
CarlosCedeniio <caencede@espol.edu.ec>
2024-01-03 10:12:31 -0500

    build

--- src/app/_services/checklist.service.ts
+++ src/app/_services/checklist.service.ts
@@ -2,7 +2,7 @@ import { Injectable } from '@angular/core';
 import { HttpClient, HttpHeaders } from '@angular/common/http';
 import { Observable } from 'rxjs';
 
-const API_URL = 'http://localhost:8080/api/checklist/';
+const API_URL = 'https://smartx.espol.edu.ec/clubapi/checklist/';
 
 const httpOptions = {
   headers: new HttpHeaders({ 'Content-Type': 'application/json' })
